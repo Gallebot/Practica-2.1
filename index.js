316bd715df6773dc0a623ff92004c19ccdd4d8ae
Gallebot <luislg2014.lggp7@gmail.com>
2024-04-23 00:33:43 -0600

    Commit metodos http

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-
 const express = require("express");
-const app = express()
+const app = express();
 
-app.get("/hola/", (req,res) =>{ //Estamos generando un subpagina
-    res.send("Hola, esta es una subpagina")
+// Ruta raíz con método GET
+app.get("/", (req, res) => {
+    res.send("Hello Profe");
 });
 
-app.get("/", (req,res) =>{ //Aqui se va a la página principal
-    res.send("Hello Profe")
+// Ruta raíz con método POST
+app.post("/", (req, res) => {
+    res.send("Has enviado un POST a la ruta raíz");
 });
 
-app.listen(8000, () =>{ //Aquí agrega la ubicación del puerto
-    console.log("Example app listening on port 8000!")
+// Subpágina '/hola' con método GET
+app.get("/hola/", (req, res) => {
+    res.send("Hola, esta es una subpágina");
 });
 
-/*
-*/
+// Subpágina '/hola' con método POST
+app.post("/hola/", (req, res) => {
+    res.send("Has enviado un POST a /hola");
+});
 
-
+// Listener del servidor en el puerto 8000
+app.listen(8000, () => {
+    console.log("Example app listening on port 8000!");
+});
