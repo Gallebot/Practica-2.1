fef000af7c8ad112a76717d7260a6300ea7f5253
“samuel” <samtorez84@gmail.com>
2024-04-22 18:06:27 -0600

    commit Samuel 2

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+
 const express = require("express");
 const app = express()
 
-app.get("/", (req,res) =>{
+app.get("/hola/", (req,res) =>{ //Estamos generando un subpagina
+    res.send("Hola")
+});
+
+app.get("/", (req,res) =>{ //Aqui se va a la página principal
     res.send("Hello Profe")
 });
 
-app.listen(8000, () =>{
+app.listen(8000, () =>{ //Aquí agrega la ubicación del puerto
     console.log("Example app listening on port 8000!")
 });
+
+/*
+*/
+
 
