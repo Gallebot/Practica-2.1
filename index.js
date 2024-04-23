9e89dce8c80704d529c78e47e279340639aa50ab
Gallebot <luislg2014.lggp7@gmail.com>
2024-04-23 00:41:03 -0600

    Codigo 200

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const app = express();
 
 // Ruta raíz con método GET
 app.get("/", (req, res) => {
-    res.send("Hello Profe");
+    res.status(200).send("Hello Profe");
 });
 
 // Ruta raíz con método POST
 app.post("/", (req, res) => {
-    res.json({ mensaje: "Has enviado un POST a la ruta raíz", numero: Math.random() });
+    res.status(200).json({ mensaje: "Has enviado un POST a la ruta raíz", numero: Math.random() });
 });
 
 // Subpágina '/hola' con método GET
 app.get("/hola/", (req, res) => {
-    res.send("Hola, esta es una subpágina");
+    res.status(200).send("Hola, esta es una subpágina");
 });
 
 // Subpágina '/hola' con método POST
 app.post("/hola/", (req, res) => {
-    res.json({ mensaje: "Has enviado un POST a /hola", numero: Math.random() });
+    res.status(200).json({ mensaje: "Has enviado un POST a /hola", numero: Math.random() });
 });
 
 // Listener del servidor en el puerto 8000
